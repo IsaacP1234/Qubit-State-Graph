e39e332cd0d95345c042a9ef15aa5575aee7f989
isaac patterson <isaacthepatterson@gmail.com>
2023-09-25 18:40:05 -0400

    added 192 edges to megagraph-unsure if correct

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import networkx as nx
 import itertools as its
+import matplotlib.pyplot as plt
 import megagraph as mg
 
 
@@ -8,5 +9,10 @@ G = nx.Graph()
 G.add_nodes_from(nodes)
 megagraph = mg.create_megagraph(G)
 print(megagraph)
-
-print(mg.similar_check(((1,2),(3,4)), ((1,2), (3,4),(2,3))))
+mg.add_flip_edges(megagraph)
+print(megagraph)
+print(mg.flip_check(((1,2),(3,4)), ((1,2), (3,4),(2,3))))
+#print(mg.pair_partitions(mg.node_pairs(4)))
+pos = nx.spring_layout(megagraph, seed = 1)
+nx.draw(megagraph, pos=pos, with_labels=True)
+plt.show()
--- megagraph.py
+++ megagraph.py
@@ -6,37 +6,36 @@ import itertools as its
 #function that returns list of unordered pairs  
 def node_pairs(n):
     pairs = []
-    for i in its.combinations(range(n), 2):
+    for i in its.combinations(range(1, n+1), 2):
         pairs.append(i)
 
     return pairs
 
 
 #function that returns a list of all possible permutations of a set of pairs(a list of tuples of tuples)
-def pair_permutations(pairs):
-    perms = []
-    for i in its.permutations(pairs):
-        perms.append(i)
-
-    return perms
+def pair_partitions(pairs):
+    parts = []
+    for i in range(1, len(pairs)+1):
+        for j in its.combinations(pairs, i):
+            parts.append(j)
+    return parts
 
 
 #for each permutation, create a node in a megagraph of that permuation of node-pairs having edges between them
 def create_megagraph(graph):
     megagraph = nx.Graph()
-    megagraph.add_node(graph)
-    permuations = pair_permutations(node_pairs(len(graph.nodes())))
+    megagraph.add_nodes_from([(graph, {"perm" : (())})])
+    permuations = pair_partitions(node_pairs(len(graph.nodes())))
     for i in range(len(permuations)):
         for j in range(len(permuations[i])):
-            for k in range(len(permuations[i][j])-1):
-                graph.add_edge(permuations[i][j][k], permuations[i][j][k+1])
+            graph.add_edge(permuations[i][j][0], permuations[i][j][1])
         state = graph.copy()
-        megagraph.add_node(state)
+        megagraph.add_nodes_from([(state, {"perm" : permuations[i]})])
         graph.clear_edges()    
     return megagraph
 
 #returns if two tuples of tuples have only one difference between them(so if 2 nodes should be connected)
-def similar_check(node1, node2):
+def flip_check(node1, node2):
     diffs = 0
     # checks if its possible for them to be similar. unnecessary but increases efficency
     if len(node1) +1 == len(node2) or len(node1) -1 == len(node2):
@@ -48,3 +47,11 @@ def similar_check(node1, node2):
                 diffs +=1
     return diffs == 1
 
+def add_flip_edges(megagraph):
+    for i in megagraph.nodes():
+        #print(megagraph.nodes[i].get("perm"))
+        for j in megagraph.nodes():
+            
+            if flip_check(megagraph.nodes[i].get("perm"), megagraph.nodes[j].get("perm")):
+                #print("b")
+                megagraph.add_edge(i, j)

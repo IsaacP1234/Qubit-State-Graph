95be7ba658c7f35f376741bc39271d18b5a9a521
isaac patterson <isaacthepatterson@gmail.com>
2023-10-18 12:36:32 -0400

    added analytics/shortest path+choose 2

--- main.py
+++ main.py
@@ -2,11 +2,18 @@ import networkx as nx
 import itertools as its
 import matplotlib.pyplot as plt
 import megagraph as mg
+import analytics as ats
 import test 
 
-#for testing. can handle  up to 5 nodes in reasonable amount of time(like 10 mins for 5, quickly for 4). 
+#for testing. can handle  up to 5 nodes in reasonable amount of time(about 13 mins for 5, 2s  for 4). 
 # can create a 6 node megagraph quickly, but adding edges takes a while. 
 # likely cant handle anything higher
+
+print(mg.lc_check([(1,4),(2,4),(1,2)],[(1,4), (1,2)], 4))
+print(mg.lc_check([(1,4), (1,2)], [(1,4),(2,4),(1,2)], 4))
+print(mg.cnot_check([(1,4), (2,4), (3,4)], [(1,4), (2,4), (3,4), (1,2), (1,3)], 4))
+print(mg.cnot_check([(1,4), (2,4), (3,4), (1,2), (1,3)], [(1,4), (2,4), (3,4)], 4))
+
 num_nodes = 4
 G = nx.Graph()
 for i in range(1, num_nodes+1):
@@ -17,18 +24,11 @@ print(megagraph)
 mg.add_edges(megagraph, num_nodes)
 print(megagraph) # correct num edges and nodes
 
-#worst case big O
-shortest_paths = nx.shortest_path_length(megagraph, G)
-worst_cases = {}
-for i in shortest_paths:
-    if shortest_paths[i] == max(shortest_paths.values()):
-        worst_cases[i] = shortest_paths[i]
-for i in worst_cases:
-    #nx.draw(i) shows them in different orders everytime and shows square twice, x once, should be vice versa
-    #plt.show()
-    print(i.adj)
-    print(worst_cases[i])#4 is longest for 4 nodes, 6 is longest for 5
-print(len(worst_cases))
+#4 is longest for 4 nodes, 6 is longest for 5
+shortest_paths_of_worst_cases = ats.find_shortest_paths_of_worst_cases(megagraph, G)
+for i in shortest_paths_of_worst_cases[0]:
+    #print(i[0])
+    print(megagraph.nodes[i[1]].get("combo"))
 #tests(just lc and cnot for now)
 test.ut.main()
 
